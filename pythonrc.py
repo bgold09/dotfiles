05700551de515a098fc31487adca11e1a5562271
Brian Golden <brian.golden09@gmail.com>
2014-03-20 19:52:14 -0400

    minor cleanup

--- pythonrc.py
+++ pythonrc.py
@@ -1,11 +1,3 @@
-# try:
-# 	import readline
-# except ImportError:
-# 	print "Module readline not available"
-# else:
-# 	import rlcompleter
-# 	readline.parse_and_bind("tab: complete")
-
 import readline, rlcompleter
 
 class irlcompleter(rlcompleter.Completer):
