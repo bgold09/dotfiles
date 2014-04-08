b23f710c963aefc64718a7029c647eed871f73ce
Brian Golden <brian.golden09@gmail.com>
2014-04-08 11:47:05 -0400

    install dependencies during setup

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+current="$(pwd)"
+
 # files to link in home directory
-declare -a files=()
 backup_dir="$HOME/dotfiles-backup-`date +%s`"
-current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 declare -a DIRS=('git' 'python')
 declare -a DIRSINSTALL=('bash' 'dotvim')
 
@@ -46,12 +48,114 @@ function place_files() {
 	done
 }
 
-echo The following configurations will be overwritten by this script:
-echo "  bash, git, python, vim"
-read -r -p "Would you like to proceed? [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+# Logging
+function e_header() { echo -e "\033[1m$@\033[0m"; }
+function e_success() { echo -e " \033[1;32m✔\033[0m $@"; }
+function e_error() { echo -e " \033[1;31m✖\033[0m $@"; }
+function e_arrow() { echo -e " \033[1;33m➜\033[0m $@"; }
+
+install_dependencies() {
+	if [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
+		install_dependencies_ubuntu
+	elif [[ "$OSTYPE" =~ ^darwin ]]; then
+		install_dependencies_osx
+	else 
+		dependencies_needed
+		exit 1
+	fi
+}
+
+install_dependencies_ubuntu() {
+	install_cmd="sudo apt-get -y install"
+	version="$(lsb_release -r | cut -f2)"
+
+	if [ ! "$(type -P git)" ]; then
+		e_header "Installing git..."
+		$install_cmd git-core
+		e_success "git installed"
+	fi
+
+	if [ ! "$(type -P ctags-exuberant)" ]; then
+		e_header "Installing ctags-exuberant..."
+		$install_cmd ctags-exuberant
+		e_success "ctags-exuberant installed"
+	fi
+
+	if [ ! "$(type -P ag)" ]; then
+		e_header "Installing ag (the_silver_searcher)..."
+		if [ $(echo "$version >= 13.10" | bc) -ne 0 ]; then
+			$install_cmd silversearcher-ag
+		else
+			for pkg in "automake" "pkg-config" "libpcre3-dev" "zlib1g-dev" "liblzma-dev"; do 
+				e_header "Installing $pkg..."
+				$install_cmd $pkg
+				e_success "$pkg installed"
+			done
+
+			git clone https://github.com/ggreer/the_silver_searcher /tmp/silver
+			pushd /tmp/silver
+			./build.sh
+			sudo make install
+			popd
+		fi
+		e_success "ag (the_silver_searcher) installed"
+	fi
+}
+
+install_dependencies_osx() {
+	install_cmd="brew install"
+
+	if [ ! "$(type -P gcc)" ]; then
+		e_error "XCode or the Command Line Tools for XCode must be installed first"
+		exit 1
+	fi
+
+	if [ ! "$(type -P ctags-exuberant)" ]; then
+		e_header "Installing ctags-exuberant..."
+		$install_cmd ctags-exuberant
+		e_success "ctags-exuberant installed"
+	fi
+
+	if [ ! "$(type -P ag)" ]; then
+		e_header "Installing ag (the_silver_searcher)..."
+		$install_cmd the_silver_searcher
+		e_success "ag (the_silver_searcher) installed"
+	fi
+}
+
+dependencies_needed() {
+	fail=
+	pkgs=
+	for pkg in "git" "ctags-exuberant"; do
+		if [ ! "$(type -P $pkg)" ]; then
+			fail=1
+			pkgs="$pkgs $pkg"
+		fi
+	done
+
+	if [ $fail -eq 1 ]; then
+		e_error "You must install the following dependencies:"
+		echo $pkgs
+		echo "Install these, then run this script again."
+		exit 1
+	fi
+
+}
+
+bootstrap() {
+	install_dependencies
 	place_files
-	echo run \`source ~/.bashrc\' to reload your bashrc file
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	bootstrap
+else 
+	echo This script may overwrite files in your home directory.
+	read -r -p "Would you like to proceed? [y/N] " response
+	if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+		bootstrap
+		echo run \`source ~/.bashrc\' to reload your bashrc file
+	fi
 fi
 
 exit 0
