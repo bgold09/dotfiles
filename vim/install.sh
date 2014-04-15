0a9ef486274559800932bcceafd2142a2ccce4be
Brian Golden <brian.golden09@gmail.com>
2014-04-15 13:00:33 -0400

    add vim config (was a submodule)

---
+++ vim/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+curr="$(pwd)"
+name="vim"
+
+place_files() {
+	ln -f vimrc $HOME/.vimrc
+	rm -f $HOME/.vim         # remove any symlink to vim config directory
+	ln -s $curr $HOME/.vim
+}
+
+function e_header() { echo -e "\033[1m$@\033[0m"; }
+function e_success() { echo -e " \033[1;32m✔\033[0m $@"; }
+function e_error() { echo -e " \033[1;31m✖\033[0m $@"; }
+function e_arrow() { echo -e " \033[1;33m➜\033[0m $@"; }
+
+e_header "Installing $name configration..."
+place_files
+e_success "$name confguration installed"
+exit 0
