074aec3d2b84872f421562204d7aaaa2f83f0f58
Brian Golden <brian.golden09@gmail.com>
2014-04-26 14:21:56 -0400

    add windows/mintty config

---
+++ windows/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+source $DOTFILES_REPO/script/helpers.bash
+
+curr="$(pwd)"
+name="$(basename $(pwd))"
+
+place_files() {
+	for file in $(ls -I 'install.sh'); do
+		ln -f $file $HOME/.$file
+	done
+}
+
+info "Installing $name configuration..."
+if [ $(uname -o) = "Cygwin" ]; then 
+	place_files
+	success "$name configuration installed"
+else 
+	fail "not on a Windows system"
+fi
+
+exit 0
