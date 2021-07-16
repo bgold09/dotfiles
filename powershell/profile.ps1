54b399658eac3e4524b920b3b0980ceeb2de6ecd
Brian Golden <bgold09@users.noreply.github.com>
2021-07-15 18:07:31 -0700

    winget: add package list, CLI settings, tab completion

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -67,6 +67,16 @@ Import-Module posh-git
 $GitPromptSettings.DefaultPromptPath = ""
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = "`n"
 
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 $localConfigPath = ".\\localConfig.ps1"
 if ((Test-Path $localConfigPath))
 {
