c03ba38fe0a9d28f77d7d4425c589f41e63ac3b0
Brian Golden <bgold09@users.noreply.github.com>
2021-10-08 14:50:23 -0700

    reorg windows-only bootstrap steps

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -26,6 +26,8 @@ function enableWindowsFeature {
     }
 }
 
+$dotPath = "$env:HOME\.dotfiles"
+
 Install-PackageProvider -Name NuGet -Force
 
 ## Trust the PowerShell Gallery repository
@@ -47,56 +49,51 @@ foreach ($psModuleName in $psModules) {
     }
 }
 
-# Install chocolatey
-if ($null -eq (Get-Command -ErrorAction SilentlyContinue -Name choco)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if ($IsWindows) {
+    # should probably do this before symlink step so that any paths to dotfiles already exist
+    Write-Host "Installing packages with winget..."
+    winget install $dotPath\windows\winget-packages.json
+
+    # Install chocolatey
+    if ($null -eq (Get-Command -ErrorAction SilentlyContinue -Name choco)) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+
+    # install choco packages
+    choco install -y $dotPath\windows\chocolatey-packages.config
+
+    # is this still needed if installing WSL via wsl.exe?
+    $computerInfo = Get-CimInstance -ClassName Win32_ComputerSystem
+    if ($computerInfo.Model -eq "Virtual Machine") {
+        enableWindowsFeature "Microsoft-Hyper-V-All"
+    }
+
+    # Set up aliases if we ever need a cmd prompt
+    New-ItemProperty -Force -PropertyType REG_SZ -Path "HKCU\Software\Microsoft\Command Processor" `
+        -Name AutoRun -Value "$dotPath\windows\win32-rc.cmd"
+
+    # see http://www.experts-exchange.com/OS/Microsoft_Operating_Systems/Windows/A_2155-Keyboard-Remapping-CAPSLOCK-to-Ctrl-and-Beyond.html
+    # http://msdn.microsoft.com/en-us/windows/hardware/gg463447.aspx
+    New-ItemProperty -Force -PropertyType REG_BINARY -Path "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" `
+        -Name "Scancode Map" -Value 0000000000000000020000001D003A0000000000
+
+    $explorerRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $virtualDesktopPinnedAppsRegPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops\PinnedApps"
+    $regKeys = @(
+        createRegKeyInfo $explorerRegPath "ShowCortanaButton" 0
+        createRegKeyInfo $explorerRegPath "HideFileExt" 0
+        createRegKeyInfo $explorerRegPath "ShowTaskViewButton" 0
+        createRegKeyInfo $explorerRegPath "ShowTaskViewButton" 0
+        createRegKeyInfo $virtualDesktopPinnedAppsRegPath "{6D809377-6AF0-444B-8957-A3773F02200E}\ConEmu\ConEmu64.exe" 0
+        createRegKeyInfo $virtualDesktopPinnedAppsRegPath "com.squirrel.Teams.Teams" 0
+        createRegKeyInfo $virtualDesktopPinnedAppsRegPath "Microsoft.WindowsTerminal_8wekyb3d8bbwe!App" 0
+    )
+
+    foreach ($item in $regKeys) {
+        setRegistryDword $item.Path $item.Name $item.Value
+    }
 }
-
-## Install Windows optional features
-$winOptionalFeatures = @(
-   "Microsoft-Windows-Subsystem-Linux"
-)
-
-foreach ($optionalFeature in $winOptionalFeatures) {
-    enableWindowsFeature $optionalFeature
-}
-
-$computerInfo = Get-CimInstance -ClassName Win32_ComputerSystem
-if ($computerInfo.Model -eq "Virtual Machine") {
-    enableWindowsFeature "Microsoft-Hyper-V-All"
-}
-
-$dotPath = "$env:HOME\.dotfiles"
-
-# should probably do this before symlink step so that any paths to dotfiles already exist
-Write-Host "Installing packages with winget..."
-winget install $dotPath\windows\winget-packages.json
 
 # install vs code extensions 
-
-# Set up aliases if we ever need a cmd prompt
-New-ItemProperty -Force -PropertyType REG_SZ -Path "HKCU\Software\Microsoft\Command Processor" `
-    -Name AutoRun -Value "$dotPath\windows\win32-rc.cmd"
-
-# see http://www.experts-exchange.com/OS/Microsoft_Operating_Systems/Windows/A_2155-Keyboard-Remapping-CAPSLOCK-to-Ctrl-and-Beyond.html
-# http://msdn.microsoft.com/en-us/windows/hardware/gg463447.aspx
-New-ItemProperty -Force -PropertyType REG_BINARY -Path "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" `
-    -Name "Scancode Map" -Value 0000000000000000020000001D003A0000000000
-
-$explorerRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$virtualDesktopPinnedAppsRegPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops\PinnedApps"
-$regKeys = @(
-    createRegKeyInfo $explorerRegPath "ShowCortanaButton" 0
-    createRegKeyInfo $explorerRegPath "HideFileExt" 0
-    createRegKeyInfo $explorerRegPath "ShowTaskViewButton" 0
-    createRegKeyInfo $explorerRegPath "ShowTaskViewButton" 0
-    createRegKeyInfo $virtualDesktopPinnedAppsRegPath "{6D809377-6AF0-444B-8957-A3773F02200E}\ConEmu\ConEmu64.exe" 0
-    createRegKeyInfo $virtualDesktopPinnedAppsRegPath "com.squirrel.Teams.Teams" 0
-    createRegKeyInfo $virtualDesktopPinnedAppsRegPath "Microsoft.WindowsTerminal_8wekyb3d8bbwe!App" 0
-)
-
-foreach ($item in $regKeys) {
-    setRegistryDword $item.Path $item.Name $item.Value
-}
