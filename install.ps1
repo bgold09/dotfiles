82f2aeb6b5eb80b8c7611743729d4fa119c0f678
Brian Golden <bgold09@users.noreply.github.com>
2021-10-27 13:14:58 -0700

    fix installation of winget pkgs at start, disambiguate by source

--- install.ps1
+++ install.ps1
@@ -1,20 +1,19 @@
-winget install Microsoft.PowerShell
+winget install --source winget --id Microsoft.PowerShell
 
 pwsh -NoProfile -NoLogo -Command {
-    winget install Microsoft.dotnet
+    winget install --source winget --id Microsoft.dotnet --version 3.1.410.15736
 
     Write-Host "Opening browser; create a GitHub PAT with 'read:packages' scope."
     Start-Process "https://github.com/settings/tokens/new"
 
     $c = Get-Credential -UserName "bgold09" -Message "Enter GitHub PAT with 'read:packages' scope."
-    $nugetSourceName = 'github-bgold09'
-    dotnet nuget add source --name $nugetSourceName "https://nuget.pkg.github.com/bgold09/index.json" `
+    dotnet nuget add source --name 'github-bgold09' "https://nuget.pkg.github.com/bgold09/index.json" `
         --username $c.UserName --password $c.GetNetworkCredential().Password
 
-    dotnet tool install --global cnct --source $nugetSourceName
+    dotnet tool install --global cnct
 
-    winget install Git.Git
-    git clone --recursive https://github.com/bgold09/dotfiles.git ~\.dotfiles
-    Set-Location ~\.dotfiles
+    winget install --source winget --id Git.Git
+    git clone --recursive https://github.com/bgold09/dotfiles.git $HOME\.dotfiles
+    Set-Location $HOME\.dotfiles
     cnct
 }
