ba33ee22bd0eb6bef3dd80310d1327fc4dd34db2
Brian Golden <bgold09@users.noreply.github.com>
2021-12-30 13:49:33 -0500

    check if user is admin when running install.ps1

--- install.ps1
+++ install.ps1
@@ -1,15 +1,23 @@
-function reloadPath() {
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
-        + ";" `
-        + [System.Environment]::GetEnvironmentVariable("Path", "User")
+if (-not [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544') {
+    Write-Host "Not running as an administrator; this script should be run in an admin session to avoid excessive UAC prompts."
+    $answer = Read-Host -Prompt "Run anyway? (Y/n)"
+    if ($answer -eq "n") {
+        exit
+    } elseif ($answer -ne "y") {
+        throw "Unrecognized input '$answer'"
+    }
 }
 
 winget install --source winget --id Microsoft.PowerShell
-reloadPath
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
+    + ";" `
+    + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
 pwsh -NoProfile -NoLogo -Command {
     winget install --source winget --id Microsoft.dotnet
-    reloadPath
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
+        + ";" `
+        + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
     Write-Host "Opening browser; create a GitHub PAT with 'read:packages' scope."
     Start-Process "https://github.com/settings/tokens/new"
