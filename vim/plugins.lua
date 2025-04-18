7bf4e2452b2da63ff07a5e490cc4d5458109061a
Brian Golden <bgold09@users.noreply.github.com>
2025-04-18 13:27:58 -0400

    vscode: skip configuring lua plugins that aren't loaded

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -1,42 +1,44 @@
-require("telescope").load_extension "frecency"
-require("telescope").setup {
-  defaults = {
-      preview = false,
-  },
-  pickers = {
-    find_files = {
-      find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
-    },
-  },
-}
+if type(vim.g.vscode) == "nil" then
+    require("telescope").load_extension "frecency"
+    require("telescope").setup {
+      defaults = {
+          preview = false,
+      },
+      pickers = {
+        find_files = {
+          find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+        },
+      },
+    }
 
-require('neo-tree').setup {
-  filesystem = {
-    window = {
-      ["/"] = "fuzzy_finder",
-    },
-  },
-}
+    require('neo-tree').setup {
+      filesystem = {
+        window = {
+          ["/"] = "fuzzy_finder",
+        },
+      },
+    }
 
-local custom_solarized = require('lualine.themes.solarized_dark')
-custom_solarized.insert.a.bg = '#b58900'
-require('lualine').setup {
-  options = { theme  = custom_solarized },
-  sections = {
-    lualine_b = {'branch', 'diagnostics'},
-  },
-  tabline = {
-    lualine_a = {'buffers'},
-  },
-}
+    local custom_solarized = require('lualine.themes.solarized_dark')
+    custom_solarized.insert.a.bg = '#b58900'
+    require('lualine').setup {
+      options = { theme  = custom_solarized },
+      sections = {
+        lualine_b = {'branch', 'diagnostics'},
+      },
+      tabline = {
+        lualine_a = {'buffers'},
+      },
+    }
 
-require("gitsigns").setup {
-  signs = {
-    add          = { text = '+' },
-    change       = { text = '~' },
-    delete       = { text = '-' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
-  },
-}
+    require("gitsigns").setup {
+      signs = {
+        add          = { text = '+' },
+        change       = { text = '~' },
+        delete       = { text = '-' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+    }
+end
