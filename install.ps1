96dcd18fee18a38825dddea3b797be4f2a2b00c5
Brian Golden <bgold09@users.noreply.github.com>
2025-09-06 18:24:24 -0400

    bootstrap: install cnct from nuget.org

--- install.ps1
+++ install.ps1
@@ -19,13 +19,6 @@ pwsh -NoProfile -NoLogo -Command {
         + ";" `
         + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-    Write-Host "Opening browser; create a GitHub PAT with 'read:packages' scope."
-    Start-Process "https://github.com/settings/tokens/new"
-
-    $c = Get-Credential -UserName "bgold09" -Message "Enter GitHub PAT with 'read:packages' scope."
-    dotnet nuget add source --name 'github-bgold09' "https://nuget.pkg.github.com/bgold09/index.json" `
-        --username $c.UserName --password $c.GetNetworkCredential().Password
-
     dotnet tool install --global cnct
     dotnet tool install --global dotnet-suggest --allow-roll-forward
 
